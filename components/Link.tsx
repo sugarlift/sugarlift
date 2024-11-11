77d0923497b3ba82c9c72bd52e8eef4648c442e7
Janne Koivistoinen <janne@sharetribe.com>
2024-11-11 19:50:34 +0200

    touch events

--- components/Link.tsx
+++ components/Link.tsx
@@ -2,7 +2,7 @@
 
 import NextLink from "next/link";
 import { useRouter } from "next/navigation";
-import { ComponentProps, MouseEvent } from "react";
+import { ComponentProps, MouseEvent, TouchEvent } from "react";
 
 type LinkProps = ComponentProps<typeof NextLink>;
 
@@ -10,18 +10,17 @@ export function Link({ children, href, onClick, ...props }: LinkProps) {
   const router = useRouter();
   const hrefString = typeof href === "object" ? href.pathname || "/" : href;
 
-  const handleMouseDown = (e: MouseEvent) => {
-    if (e.button === 0) {
-      e.preventDefault();
-      router.push(hrefString);
-    }
+  const handleActivation = (e: MouseEvent | TouchEvent) => {
+    e.preventDefault();
+    router.push(hrefString);
   };
 
   return (
     <NextLink
       href={href}
       onClick={onClick}
-      onMouseDown={handleMouseDown}
+      onMouseDown={(e) => e.button === 0 && handleActivation(e)}
+      onTouchStart={handleActivation}
       {...props}
     >
       {children}
