29f0b40f7339d3eeff3b9fbab59c2db441a2e786
Janne Koivistoinen <janne@sharetribe.com>
2024-11-22 01:24:50 +0200

    new airtable setup for artists 9

--- lib/airtable.ts
+++ lib/airtable.ts
@@ -8,14 +8,19 @@ if (!process.env.AIRTABLE_BASE_ID) {
   throw new Error("Missing AIRTABLE_BASE_ID");
 }
 
+// Store these in constants to ensure they're not undefined
+const AIRTABLE_BASE_ID = process.env.AIRTABLE_BASE_ID;
+const AIRTABLE_PERSONAL_ACCESS_TOKEN =
+  process.env.AIRTABLE_PERSONAL_ACCESS_TOKEN;
+
 const airtable = new Airtable({
-  apiKey: process.env.AIRTABLE_PERSONAL_ACCESS_TOKEN,
+  apiKey: AIRTABLE_PERSONAL_ACCESS_TOKEN,
 });
 
 export const getArtistsTable = () => {
-  return airtable.base(process.env.AIRTABLE_BASE_ID).table("tblkYraa6YhVleHVu");
+  return airtable.base(AIRTABLE_BASE_ID).table("tblkYraa6YhVleHVu");
 };
 
 export const getArtworkTable = () => {
-  return airtable.base(process.env.AIRTABLE_BASE_ID).table("tblj8MEqwAWKPnxmd");
+  return airtable.base(AIRTABLE_BASE_ID).table("tblj8MEqwAWKPnxmd");
 };
