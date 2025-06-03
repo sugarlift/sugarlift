fd9f85e40b5c787db6b53052f8b9b22b94b8b9fa
Janne Koivistoinen <janne@janne.me>
2025-06-03 13:41:16 +0300

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    unoptimized: false,
+    unoptimized: true,
     minimumCacheTTL: 60 * 60 * 24 * 7,
     remotePatterns: [
       {
